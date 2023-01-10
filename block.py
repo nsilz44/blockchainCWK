7ddcadf26311aa48efb5e931d75e610af84ac13e
Nsilz44 <n_stbss@hotmail.com>
2023-01-10 21:27:54 +0000

    created block.py

--- block.py
+++ block.py
@@ -1,1 +1,39 @@
 import numpy as np
+import webbrowser
+
+file = open('Dapp.html', 'w')
+
+# the html code which will go in the file GFG.html
+html = """
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="buidljs/buidl.js"></script>
+    <script src="qrcodejs/qrcode.js"></script>
+	</head>
+	<body>
+	<script>
+	let newPair = buidl.createFrom("");
+	let address = newPair.p2pkh;
+	let privateKey = newPair.pk;
+	console.log(address, privateKey);
+	</script>
+	<div id="qrcode"></div>
+<script type="text/javascript">
+new QRCode(document.getElementById("qrcode"), privateKey);
+</script>
+</body>
+    
+</html>
+"""
+
+# writing the code into the file
+file.write(html)
+
+# close the file
+file.close()
+
+
+
+# open html file
+webbrowser.open('GFG.html')
